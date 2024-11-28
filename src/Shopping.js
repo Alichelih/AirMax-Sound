c44931c961b06e9bc8d02f70efe92fec4b235a7a
Alichelih <alichelih12l@gmaile.com>
2024-11-29 00:54:22 +0100

    Your commit message

---
+++ src/Shopping.js
@@ -0,0 +1,10 @@
+import { plantList } from '../datas/plantList'
+function ShoppingList() {
+    return (
+    <div>
+    // Afficher les cathegories seulement
+    // afficher un emoji 🔥🔥 en fonction du champ isBestsale
+    </div>
+    )
+    }
+    export default ShoppingList
